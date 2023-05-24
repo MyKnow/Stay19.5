806347d504ebc80482e31728787dfcc902ff58c5
unknown <[Github>
2023-05-24 15:27:50 +0900

    templates

---
+++ common/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def main(request):
+    return render(request, 'common/main.html')
+
+def login(request):
+    return render(request, 'common/login.html')

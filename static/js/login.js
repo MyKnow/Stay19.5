e692de4526cafc0c4963fd11bc3e474ab53459a4
MyKnow <myknow000@gmail.com>
2023-05-29 16:41:14 +0900

    merge myknow-gang

---
+++ static/js/login.js
@@ -0,0 +1,45 @@
+Kakao.init('aa4f2cb7ffd0e8ced07032bfa9361f57'); //발급받은 키 중 javascript키를 사용해준다.
+console.log(Kakao.isInitialized()); // sdk초기화여부판단
+//카카오로그인
+function kakaoLogin() {
+    Kakao.Auth.login({
+      success: function (response) {
+        Kakao.API.request({
+          url: '/v2/user/me',
+          success: function (response) {
+        	  console.log(response)
+          },
+          fail: function (error) {
+            console.log(error)
+          },
+        })
+      },
+      fail: function (error) {
+        console.log(error)
+      },
+    })
+  }
+//카카오로그아웃  
+function kakaoLogout() {
+    if (Kakao.Auth.getAccessToken()) {
+      Kakao.API.request({
+        url: '/v1/user/unlink',
+        success: function (response) {
+        	console.log(response)
+        },
+        fail: function (error) {
+          console.log(error)
+        },
+      })
+      Kakao.Auth.setAccessToken(undefined)
+    }
+}
+
+const button = document.querySelector('login-button');
+
+const buttonClickHandler = () =>{
+    alert('Button clicked!');
+  };
+   
+  button.addEventListener('click',kakaoLogin);
+  //button.removeEventListener('click', buttonClickHandler);
